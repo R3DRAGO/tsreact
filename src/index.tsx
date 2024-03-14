a77cc77a1e6fd4ff3b560ee0a14e2d55415a38ca
bagil <bagilo.v@icloud.com>
2024-03-14 12:19:52 +0200

    lesson4

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import ReactDOM from 'react-dom/client';
+import {RouterProvider} from "react-router-dom";
+
 import './index.css';
-import {App} from './App';
+import {router} from "./router";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <Provider store={store}>
+        <RouterProvider router={router}/>
+    </Provider>
 );
 
 
